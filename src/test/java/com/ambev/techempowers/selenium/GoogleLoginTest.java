a9beae42130f4276ae43601e52ba4c45efb86e12
Emerson <griloemer@gmail.com>
2023-09-07 12:56:46 -0300

    Selenium Funcionando com webdriver manager no chrome

--- src/test/java/com/ambev/techempowers/selenium/GoogleLoginTest.java
+++ src/test/java/com/ambev/techempowers/selenium/GoogleLoginTest.java
@@ -1,11 +1,14 @@
 package com.ambev.techempowers.selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,20 +19,19 @@ public class GoogleLoginTest {
 
         @Test
         public void testLogin() {
-            // Set the path to your WebDriver executable
-            System.setProperty("webdriver.chrome.driver", "/home/emerson/Área de Trabalho/chromedriver");
 
-            // Create a Chrome WebDriver instance
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized"); // Start Chrome maximized
-            WebDriver driver = new ChromeDriver(options);
+            WebDriverManager.chromedriver().setup();
+
+            // Create a WebDriver instance
+            WebDriver driver = new ChromeDriver();
 
             // Navigate to the Google login page
-            driver.get("https://accounts.google.com");
+            driver.get("http://www.globo.com");
 
             // Find the "Email or phone" input field and enter your Google email address
-            WebElement emailInput = driver.findElement(By.id("identifierId"));
-            emailInput.sendKeys("your_email@gmail.com");
+            WebElement emailInput = driver.findElement(By.id("header-search-input"));
+            emailInput.sendKeys("java");
+            emailInput.sendKeys(Keys.RETURN );
 
             // Click the "Next" button to proceed to the password input
             WebElement nextButton = driver.findElement(By.id("identifierNext"));
